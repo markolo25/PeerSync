b32de0dac32f429ebe9b96a2a05715a05532822e
Mark Mendoza <markolo25@gmail.com>
2017-11-25 06:22:15 -0800

    Added some classes

---
+++ src/main/java/peerSync/model/transferRecieve.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peerSync.model;
+
+/**
+ *
+ * @author Mark Levie Mendoza <markolo25@gmail.com>
+ */
+public class transferRecieve {
+    
+}
