24f81a5bb88e6112a350b9d2005cd5647d1c5190
Amanda Pan <daikiraidemodaisuki@gmail.com>
2017-11-28 05:52:40 -0800

    preparing for the next step -mark

--- src/main/java/peerSync/model/peerSyncModel.java
+++ src/main/java/peerSync/model/peerSyncModel.java
@@ -2,6 +2,7 @@ package peerSync.model;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
@@ -15,12 +16,32 @@ public class peerSyncModel {
 
     private File directory;
     private ArrayList<File> filesToBeSynced;
+    private HashSet<String> remoteIPs;
 
     public peerSyncModel(String strDirectory) {     
         //get Directory
         this.directory = new File(strDirectory);       
         //get File List
-        filesToBeSynced = new ArrayList<>(FileUtils.listFiles(this.directory, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE));      
+        filesToBeSynced = new ArrayList<>(FileUtils.listFiles(this.directory, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE));
+        //start listening for peers
+        
+        //send meta data to peers
+        
+        //initialize syncing
+        
+    }
+    
+    
+    public void queueServers(){
+        
+    }
+    
+    public void queueClient(){
+        
+    }
+    
+    public void broadCast(){
+        //Insert broadcast here
     }
 
 
